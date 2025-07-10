ae750445abac4132c77ecffe9b4bce7b3b23ac02
TranDuongVTI <duong.tranthai@vti.com.vn>
2025-07-10 17:46:39 +0700

    chore: added example data.json

--- main.py
+++ main.py
@@ -2,29 +2,41 @@ from transformers import AutoTokenizer, AutoModelForTokenClassification
 import torch
 from torch.utils.data import DataLoader, Dataset
 from transformers import XLMRobertaTokenizer, XLMRobertaForTokenClassification
+# TODO:
+# Add website labe
 
-tokenizer = AutoTokenizer.from_pretrained("Davlan/xlm-roberta-base-ner-hrl")
-model = AutoModelForTokenClassification.from_pretrained("Davlan/xlm-roberta-base-ner-hrl")
+# ========== UTILITIES ==========
+def label2id(label, label_map):
 
+def label2id(label, label_map):
+
+# ========== DATASET   ==========
 class NERDataset(Dataset):
-    def __init__(self, texts, labels, tokenizer):
-        self.texts = texts
-        self.labels = labels
+    def __init__(self, json_file, tokenizer, label_map):
+        self.texts = []
+        self.labels = []
         self.tokenizer = tokenizer
-    
+        self.label_map = label_map
+        
+        # Tokenize data
+        # Add a TQDM loader
+
     def __len__(self):
         return len(self.texts)
     
     def __getitem__(self, idx):
         pass
-    
+
 class NERDataLoader(DataLoader):
     def __init__ (self, dataset, batch_size=32, shuffle=True):
         super().__init__(dataset, batch_size=batch_size, shuffle=shuffle)
+        self.tokenizer = dataset.tokenizer
+        self.data_collator = DataCollatorForTokenClassification(tokenizer=self.tokenizer)
     
     def collate_fn(self, batch):
-        pass
+        return self.data_collator(batch)
 
+# ========== MODEL   ==========
 class NERModel(torch.nn.Module):
     def __init__(self, model):
         super(NERModel, self).__init__()
@@ -34,4 +46,6 @@ class NERModel(torch.nn.Module):
         pass
 
 if __name__ == '__main__':
-    pass
+    tokenizer = AutoTokenizer.from_pretrained("Davlan/xlm-roberta-base-ner-hrl")
+    model = AutoModelForTokenClassification.from_pretrained("Davlan/xlm-roberta-base-ner-hrl")
+    
